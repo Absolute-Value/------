780a0bee9a93856a39483d3a1aa89260e852417b
Absolute-Value <ji.kei1220@gmail.com>
2023-05-12 19:42:25 +0900

    ボスは一旦削除

--- RPG.py
+++ RPG.py
@@ -3,116 +3,77 @@ import random
 class Player:
     def __init__(self):
         self.health = 5
-        self.level = 1
         self.attack_power = 1
+        self.level = 1
 
     def level_up(self):
         self.level += 1
         self.attack_power += 1
-        print("Level up! Player's level is now", self.level)
+        print("Level Up! Your attack power increased to", self.attack_power)
 
-class Enemy:
-    def __init__(self, x, y):
-        self.health = 1
-        self.x = x
-        self.y = y
-
-    def move(self, player_position):
-        dx = player_position[0] - self.x
-        dy = player_position[1] - self.y
-
-        if dx != 0:
-            self.x += dx // abs(dx)
-        if dy != 0:
-            self.y += dy // abs(dy)
-
-class Boss:
-    def __init__(self, x, y):
-        self.health = 10
-        self.x = x
-        self.y = y
 
 class Game:
     def __init__(self, map_size):
-        self.player = Player()
         self.map_size = map_size
-        self.map = [["-" for _ in range(map_size)] for _ in range(map_size)]
-        self.player_position = (0, 0)
+        self.map = [[' ' for _ in range(self.map_size)] for _ in range(self.map_size)]
+        self.player = Player()
+        self.player_position = None
         self.enemies = []
-        self.boss = None
-        self.generate_enemies()
-        self.generate_boss()
         self.place_entities()
+        self.game_over = False
 
-    def generate_enemies(self):
+    def place_entities(self):
+        self.place_player()
+        self.place_enemies()
+
+    def place_player(self):
+        x = random.randint(0, self.map_size - 1)
+        y = random.randint(0, self.map_size - 1)
+        self.player_position = (x, y)
+        self.map[x][y] = 'P'
+
+    def place_enemies(self):
         for _ in range(5):
             while True:
                 x = random.randint(0, self.map_size - 1)
                 y = random.randint(0, self.map_size - 1)
-                if self.map[x][y] == "-" and (x, y) != self.player_position:
-                    enemy = Enemy(x, y)
-                    self.enemies.append(enemy)
+                if self.map[x][y] == ' ':
+                    self.map[x][y] = 'E'
+                    self.enemies.append((x, y))
                     break
 
-    def generate_boss(self):
-        while True:
-            x = random.randint(0, self.map_size - 1)
-            y = random.randint(0, self.map_size - 1)
-            if self.map[x][y] == "-" and (x, y) != self.player_position:
-                self.boss = Boss(x, y)
-                break
-
-    def place_entities(self):
-        self.map[self.player_position[0]][self.player_position[1]] = "P"
-        for enemy in self.enemies:
-            self.map[enemy.x][enemy.y] = "E"
-        if self.boss:
-            self.map[self.boss.x][self.boss.y] = "B"
-
     def print_map(self):
-        for i in range(self.map_size):
-            for j in range(self.map_size):
-                print(self.map[i][j], end=" ")
-            print()
-        print("Player HP:", self.player.health)
-        print("Player Level:", self.player.level)
+        for row in self.map:
+            print(' '.join(row))
 
     def move_player(self, direction):
         x, y = self.player_position
-        if direction == "w" and x > 0:
-            self.map[x][y] = "-"
+
+        if direction == 'w' and x > 0:
             x -= 1
-        elif direction == "s" and x < self.map_size - 1:
-            self.map[x][y] = "-"
+        elif direction == 's' and x < self.map_size - 1:
             x += 1
-        elif direction == "a" and y > 0:
-            self.map[x][y] = "-"
+        elif direction == 'a' and y > 0:
             y -= 1
-        elif direction == "d" and y < self.map_size - 1:
-            self.map[x][y] = "-"
+        elif direction == 'd' and y < self.map_size - 1:
             y += 1
 
-        self.player_position = (x, y)
-        self.map[x][y] = "P"
+        if self.map[x][y] == 'E':
+            self.encounter_enemy(x, y)
+        elif self.map[x][y] == ' ':
+            self.map[self.player_position[0]][self.player_position[1]] = ' '
+            self.player_position = (x, y)
+            self.map[x][y] = 'P'
 
-        self.check_encounter()
+    def encounter_enemy(self, x, y):
+        print("Enemy encountered!")
+        self.attack_enemy(x, y)
 
-    def check_encounter(self):
-        x, y = self.player_position
+    def attack_enemy(self, x, y):
+        enemy_index = self.enemies.index((x, y))
+        enemy = self.enemies.pop(enemy_index)
 
-        # Check if the player encounters an enemy
-        for enemy in self.enemies:
-            if enemy.x == x and enemy.y == y:
-                self.attack_enemy(enemy)
-                break
-
-        # Check if the player encounters the boss
-        if self.boss and self.boss.x == x and self.boss.y == y:
-            self.attack_boss()
-
-    def attack_enemy(self, enemy):
         while True:
-            print("Enemy encountered!")
             print("1. Physical Attack")
             print("2. Defend")
             print("3. Magic Attack")
@@ -129,72 +90,32 @@ class Game:
             else:
                 print("Invalid choice! Please try again.")
 
-    def attack_boss(self):
-        while True:
-            print("Boss encountered!")
-            print("1. Physical Attack")
-            print("2. Defend")
-            print("3. Magic Attack")
-            choice = input("Choose your action (1-3): ")
-            if choice == "1":
-                self.physical_attack_boss()
-                break
-            elif choice == "2":
-                self.defend()
-                break
-            elif choice == "3":
-                self.magic_attack_boss()
-                break
-            else:
-                print("Invalid choice! Please try again.")
-
     def physical_attack(self, enemy):
-        enemy.health -= self.player.attack_power
-
-        if enemy.health <= 0:
-            self.enemies.remove(enemy)
-            print("You defeated an enemy!")
-            self.player.level_up()
-        else:
-            print("You attacked an enemy. Enemy's health:", enemy.health)
-
-    def physical_attack_boss(self):
-        self.boss.health -= self.player.attack_power
-
-        if self.boss.health <= 0:
-            print("Congratulations! You defeated the boss!")
-            self.player.level_up()
-        else:
-            print("You attacked the boss. Boss's health:", self.boss.health)
+        self.map[enemy[0]][enemy[1]] = ' '
+        print("You defeated an enemy!")
+        self.player.level_up()
 
     def defend(self):
         print("You defended against the enemy's attack!")
         # Apply any defensive effects or calculations here
 
     def magic_attack(self, enemy):
-        # Implement the logic for magic attack against an enemy here
         print("Magic attack against an enemy is not implemented yet!")
 
-    def magic_attack_boss(self):
-        # Implement the logic for magic attack against the boss here
-        print("Magic attack against the boss is not implemented yet!")
+    def run_game(self):
+        while not self.game_over:
+            self.print_map()
+            command = input("Press 'w' to move up, 's' to move down, 'a' to move left, 'd' to move right: ")
+            if command in ["w", "s", "a", "d"]:
+                self.move_player(command)
+            else:
+                print("Invalid command!")
 
-    def attack_boss(self):
-        self.boss.health -= self.player.attack_power
+    def game_over(self):
+        print("Game Over")
+        self.game_over = True
 
-        if self.boss.health <= 0:
-            print("Congratulations! You defeated the boss!")
-            self.player.level_up()
-        else:
-            print("You attacked the boss. Boss's health:", self.boss.health)
-
-map_size = 5  # マップサイズを設定
+# ゲームの開始
+map_size = 10  # マップサイズを設定
 game = Game(map_size)
-
-while game.player.health > 0 and game.boss:
-    game.print_map()
-    command = input("Press 'w' to move up, 's' to move down, 'a' to move left, 'd' to move right: ")
-    if command in ["w", "s", "a", "d"]:
-        game.move_player(command)
-    else:
-        print("Invalid command!")
+game.run_game()
