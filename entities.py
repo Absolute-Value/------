d3a8a98661a48d05e7fefd5235d517af7e12a435
Absolute-Value <ji.kei1220@gmail.com>
2023-05-15 06:53:47 +0900

    ミスを修正

--- entities.py
+++ entities.py
@@ -26,4 +26,4 @@ class Enemy(Entity):
             
 class Boss(Enemy):
      def __init__(self, x:int, y:int, name:str="BoneKing", health:int=10, attack_power:int=3, exp:int=5, escape_rate:float=0, drop_items:dict={"ポーション":0.5}):
-        super().__init__(x, y, name, health, attack_power, exp, escape_rate)
+        super().__init__(x, y, name, health, attack_power, exp, escape_rate, drop_items)
